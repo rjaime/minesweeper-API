5efd0b7d59f9d294448f9f57cbd640b40dd77477
Rodrigo Jaime <rodrigojaime57@gmail.com>
2019-12-06 11:38:35 -0300

    Updated views and url. Missing templates.

--- django_minesweeper_api/core/urls.py
+++ django_minesweeper_api/core/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
 from rest_framework import routers
 
 from . import views
@@ -10,6 +12,12 @@ router.register(r'matches',
     base_name='matches'
 )
 
+api_urls = [
+    url(r'^api/', include(router.urls)),
+]
+
 urlpatterns = [
-    *router.urls,
+    *api_urls,
+    url(r'^$', TemplateView.as_view(template_name="lobby.html"), name='lobby'),
+    url(r'^match/(?P<match_id>[0-9]+)/$', TemplateView.as_view(template_name="game.html"), name='match_game'),
 ]
--- django_minesweeper_api/core/views.py
+++ django_minesweeper_api/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
+from django.views import View
 
-from rest_framework import permissions as drf_permissions
+from rest_framework import (
+    exceptions as drf_exceptions,
+    permissions as drf_permissions,
+)
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -35,16 +40,80 @@ class MatchViewSet(
 
     @action(detail=True, methods=['post'])
     def reveal(self, request, pk=None):
+        import pdb; pdb.set_trace()
+        x, y = self.extract_coords(request.data)
+
+        try:
+            match = Match.objects.get(id=pk)  # pylint: disable=no-member
+        except Match.DoesNotExist:
+            return Response(
+                {'message': _("Match id {} not found.".format(pk))},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        try:
+            game_status, cells_revealed = match.reveal(x, y)
+        except Exception as ex:
+            raise drf_exceptions.APIException(str(ex))
+
         response_data = {
-            'target': [0, 0],
-            'cells': {},
+            'target': [x, y],
+            'game_status': game_status,
+            'cells': cells_revealed,
         }
         return Response(response_data, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['post'])
     def flag(self, request, pk=None):
+        x, y = self.extract_coords(request.data)
+
+        try:
+            match = Match.objects.get(id=pk)  # pylint: disable=no-member
+        except Match.DoesNotExist:
+            return Response(
+                {'message': _("Match id {} not found.".format(pk))},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        try:
+            game_status, cells_revealed = match.flag(x, y)
+        except Exception as ex:
+            raise drf_exceptions.APIException(str(ex))
+
         response_data = {
-            'target': [0, 0],
-            'cells': {},
+            'target': [x, y],
+            'game_status': game_status,
+            'cells': cells_revealed,
         }
         return Response(response_data, status=status.HTTP_200_OK)
+
+    def extract_coords(self, req_data):
+        x = req_data.get('x', None)
+        
+        if x is None:
+            raise drf_exceptions.ValidationError(_("'x' value is missing."))
+
+        y = req_data.get('y', None)
+        
+        if y is None:
+            raise drf_exceptions.ValidationError(_("'y' value is missing."))
+
+        if not isinstance(x, int):
+            raise drf_exceptions.ValidationError(_("'x' must be integer"))
+
+        if not isinstance(y, int):
+            raise drf_exceptions.ValidationError(_("'y' must be integer"))
+
+        if x < 0:
+            raise drf_exceptions.ValidationError(_("'x' must be zero or greater."))
+
+        if y < 0:
+            raise drf_exceptions.ValidationError(_("'y' must be zero or greater."))
+
+        return x, y
+
+
+class GameView(View):
+    def get(self, request, match_id):
+        context = {'match_id': match_id}
+        return render(request, 'game.html', context)
